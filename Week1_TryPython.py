9f8a98995c47d86d35c78fc6b688eaf94869242c
Tasha <twilson0423@gmail.com>
2017-02-15 01:23:57 -0600

    added new files

---
+++ Week1_TryPython.py
@@ -0,0 +1,7 @@
+x = Kinry
+print x
+x = Carrillo
+print x
+y = Enrique
+x + y 
+print x + y
---
+++ print_comments.py
@@ -0,0 +1,4 @@
+print "read below for more multi-line comments in python" #this would execute
+#This line and below would not execute
+
+
