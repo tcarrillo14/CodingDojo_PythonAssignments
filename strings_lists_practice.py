e256f77bf945b84ee53636b9af72c4c4bd39ece8
Tasha <twilson0423@gmail.com>
2017-02-15 21:15:08 -0600

    added in new strings_lists file

---
+++ strings_lists_practice.py
@@ -0,0 +1,22 @@
+print "python"
+#Find and Replace
+my_string = "If monkeys like bananas, then I must be a monkey!"
+print my_string.find("monkey")
+print my_string.replace("monkey", "alligator", 3)
+
+#Min and Max
+my_list = [2,54,-2,7,12,98]
+print max(my_list)
+my_list = [2,54,-2,7,12,98]
+print min(my_list)
+
+#First and Last
+x = ["hello", 2,54,-2,7,12,98,"world"]
+print x[0]
+print x[7]
+
+#New List
+my_list2 = [19,2,54,-2,7,12,98,32,10,-3,6]
+my_list2.sort()
+print my_list2
+print my_list2[:3]
